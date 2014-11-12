911d9553d8492712620b005ba6878e8ad1b81211
Sergio Barrera <sdbeng@gmail.com>
2014-11-12 09:24:52 -0800

    Update movies_night.rb beta ver.

--- movies_night.rb
+++ movies_night.rb
@@ -5,16 +5,17 @@ choice = gets.chomp
 case choice
 when "add"
 #if the movie is not already in the hash, add it:
-if movies[title.to_sym].nil?
+
   puts "Enter a movie title:"
   title = gets.chomp
-  puts "What your movie's rating- (0-5)?"
-  rating = gets.chomp
-  movies[title.to_sym] = rating.to_i
-  puts "The movie #{title} with rating #{rating} was successfully added."
-else
+  if movies[title.to_sym].nil?
+    puts "What your movie's rating- (0-5)?"
+    rating = gets.chomp
+    movies[title.to_sym] = rating.to_i
+    puts "The movie #{title} with rating #{rating} was successfully added."
+  else
     puts "This movie already exists in our database."
-end
+  end
 when "update"
   puts "Updated!"
 when "display"
