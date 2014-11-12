f3ac680df324d6529af45ab100bf10218ccaf55a
Sergio Barrera <sdbeng@gmail.com>
2014-11-12 10:22:20 -0800

    Update movie_night v1

--- movies_night.rb
+++ movies_night.rb
@@ -5,23 +5,44 @@ choice = gets.chomp
 case choice
 when "add"
 #if the movie is not already in the hash, add it:
-
   puts "Enter a movie title:"
   title = gets.chomp
+  #if the movie's title is nil, add it up
   if movies[title.to_sym].nil?
-    puts "What your movie's rating- (0-5)?"
-    rating = gets.chomp
-    movies[title.to_sym] = rating.to_i
-    puts "The movie #{title} with rating #{rating} was successfully added."
-  else
+ 
+  puts "What your movie's rating- (0-5)?"
+  rating = gets.chomp
+  movies[title.to_sym] = rating.to_i
+  puts "The movie #{title} with rating #{rating} was successfully added."
+else
     puts "This movie already exists in our database."
-  end
+end
 when "update"
-  puts "Updated!"
+  puts "Enter a movie title:"
+  title = gets.chomp
+  #if the movie's title is nil, then movie is not in hash
+  if movies[title.to_sym].nil?
+      puts "Sorry, that movie is not in our database. Add it in!"
+  else
+      #we need to update the movie's hash
+      puts "Enter a new rating - (0-5)"
+      rating = gets.chomp
+      #set the movie's rating to that new title
+      movies[title.to_sym] = rating.to_i
+      puts "The title #{title} has a new rating of #{rating}!"
+  end
 when "display"
-  puts "Movies!"
+  movies.each{|m,r| puts "#{m}: #{r}" }
 when "delete"
-    puts "Deleted!"
+  puts "Enter a movie title to delete:"
+  title = gets.chomp
+    if movies[title.to_sym].nil?
+        puts "Movie not in database!"
+    else
+        movies.delete(title.to_sym)
+        puts "Movie title #{title.to_sym} succesfully deleted!"
+    end
+    
 else
   puts "Error!"
 end
