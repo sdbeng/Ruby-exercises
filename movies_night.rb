30fef8817ce9aa8af944e26d0d64d58ef25e21b6
Sergio Barrera <sdbeng@gmail.com>
2014-11-12 09:20:27 -0800

    Add movies_night.rb

---
+++ movies_night.rb
@@ -0,0 +1,26 @@
+movies = {Firefox: 4, Hulk: 3, AnaFrank: 4}
+puts "What would you like to do?"
+choice = gets.chomp
+
+case choice
+when "add"
+#if the movie is not already in the hash, add it:
+if movies[title.to_sym].nil?
+  puts "Enter a movie title:"
+  title = gets.chomp
+  puts "What your movie's rating- (0-5)?"
+  rating = gets.chomp
+  movies[title.to_sym] = rating.to_i
+  puts "The movie #{title} with rating #{rating} was successfully added."
+else
+    puts "This movie already exists in our database."
+end
+when "update"
+  puts "Updated!"
+when "display"
+  puts "Movies!"
+when "delete"
+    puts "Deleted!"
+else
+  puts "Error!"
+end
