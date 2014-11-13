f5b21e77382846939fff530a0ce589614c65b359
Sergio Barrera <sdbeng@gmail.com>
2014-11-13 07:54:03 -0800

    Modify regex_code.rb

--- regex_code.rb
+++ regex_code.rb
@@ -1,3 +1,29 @@
 #basic code
-puts "Good morning, regex!"
-puts "Sup!"
+puts "Good morning, Serg! Let's try some regex."
+puts "--------------------------"
+puts "--Basic solution--"
+puts "--------------------------"
+
+def starts_with_consonant?(s)
+	regex = /\A(?=[^aeiou])([a-z])/i
+	
+	if regex =~ s
+		puts "Yes, word is correct! You entered: #{s}"
+		return true
+	else
+		puts "Nope, try again. Word entered was: #{s}"
+		return false
+	end
+end
+puts starts_with_consonant?("rlifornia")
+
+#separator
+puts "--------------------------------"
+puts "--Alternative shorter solution--"
+puts "--------------------------------"
+
+#Alternative solution with use of ternary operators
+def starts_with_consonant_alt?(s)
+	s =~ /\A(?=[^aeiou])(?=[a-z])/i ? true : false
+end
+puts starts_with_consonant_alt?("rlifornia")
